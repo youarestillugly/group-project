82935aba8f4bc6ecd92c0cd1fb27882788e56026
youarestillugly <justlittletoxic@gmail.com>
2025-06-04 16:58:28 +0600

    ground-booking

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/User');
 const Feedback = require('../models/feedbackModel');
 
 const Event = require('../models/eventModel');
